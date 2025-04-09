5ed5755dded52fa3b2c2e3b876e8b8402af2af96
tessertitan1122 <tessertitan1122@gmail.com>
2025-04-09 12:05:31 +0700

    .

--- Knn.py
+++ Knn.py
@@ -19,3 +19,12 @@ with col2:
 with col3:
    st.header("Setosa")
    st.image("./img/iris3.jpg")
+
+html_7 = """
+<div style="background-color:#c5f18a;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
+<center><h5>ข้อมูล iris หรือข้อมูลดอกไม้สำหรับทำนาย</h5></center>
+</div>
+"""
+st.markdown(html_7, unsafe_allow_html=True)
+st.markdown("")
+st.markdown("")
