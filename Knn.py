e40e4c81b0298b48f13e7c55f22b5c0a1b9b6a5a
tessertitan1122 <tessertitan1122@gmail.com>
2025-04-09 12:02:53 +0700

    .

--- Knn.py
+++ Knn.py
@@ -5,4 +5,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 st.title('การจำแนกข้อมูลด้วยเทคนิค Machine Learnning')
-st.image("./img/go1.jpg")
+# st.image("./img/go1.jpg")
+col1, col2, col3 = st.columns(3)
+
+with col1:
+   st.header("Versicolor")
+   st.image("./img/iris1.jpg")
+
+with col2:
+   st.header("Verginiga")
+   st.image("./img/iris2.jpg")
+
+with col3:
+   st.header("Setosa")
+   st.image("./img/iris3.jpg")
